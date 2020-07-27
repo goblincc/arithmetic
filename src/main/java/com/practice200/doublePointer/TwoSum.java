211c6714144a31bd8ebf32fe989b0bb9f75c7f05
chenchang1 <chenchang1@yy.com>
2020-07-27 17:20:55 +0800

    sum

---
+++ src/main/java/com/practice200/doublePointer/TwoSum.java
@@ -0,0 +1,30 @@
+package com.practice200.doublePointer;
+
+/**
+ * Created by Administrator on 2020/7/27.
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] ints = twoSum(nums, 9);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+    public static int[] twoSum(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                return new int[]{left, right};
+            }else if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return null;
+    }
+
+}
