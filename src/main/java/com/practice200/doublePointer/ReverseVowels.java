ab9d90f3217d0190e4e10f1c3b999200b1f5bceb
goblincc <12chenchang@163.com>
2020-07-29 23:52:19 +0800

    'reverse'

---
+++ src/main/java/com/practice200/doublePointer/ReverseVowels.java
@@ -0,0 +1,38 @@
+package com.practice200.doublePointer;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Created by cc on 2020/7/29.
+ */
+public class ReverseVowels {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        String s1 = new ReverseVowels().reverseVowels(s);
+        System.out.println(s1);
+    }
+    private final static HashSet<Character> vowels = new HashSet<>(
+            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+    );
+
+    public String reverseVowels(String s){
+        if(s == null) return null;
+        int i = 0;
+        int j = s.length() - 1;
+        char[] result = new char[s.length()];
+        while(i < j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+            if(!vowels.contains(left)){
+                result[i++] = left;
+            }else if(!vowels.contains(right)){
+                result[j--] = right;
+            }else{
+                result[i++] = right;
+                result[j--] = left;
+            }
+        }
+        return new String(result);
+    }
+}
