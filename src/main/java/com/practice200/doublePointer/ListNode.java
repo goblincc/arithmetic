110114240ef578024e2de6ffd372d2d980ed1090
goblincc <12chenchang@163.com>
2020-08-03 00:05:29 +0800

    'hascycle'

---
+++ src/main/java/com/practice200/doublePointer/ListNode.java
@@ -0,0 +1,17 @@
+package com.practice200.doublePointer;
+
+/**
+ * Created by cc on 2020/8/2.
+ */
+public class ListNode {
+    public int value;
+    public ListNode next;
+
+    public ListNode(int value){
+        this.value = value;
+    }
+
+    public void show(){
+        System.out.println(this.value);
+    }
+}
