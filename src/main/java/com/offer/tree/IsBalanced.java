b3daf4160f617ce429215bdb2b336a82e3625ad1
chenchang1 <chenchang1@yy.com>
2021-02-07 23:15:13 +0800

    'lowest'

--- src/main/java/com/offer/tree/IsBalanced.java
+++ src/main/java/com/offer/tree/IsBalanced.java
@@ -11,8 +11,8 @@ public class IsBalanced {
     private int height(TreeNode root){
         if(root == null){
             return 0;
-        }else{
-            return 1 + Math.max(height(root.left), height((root.right)));
         }
+        return 1 + Math.max(height(root.left), height((root.right)));
+
     }
 }
